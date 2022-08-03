c0d293790f97c2f6042facd9c9e63afe409f803b
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-08-03 10:40:51 +0300

    Nothing completed.

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        for(int i = 0;i <= sideLength;i++){
+            for(int y = 0;y <= sideLength;y++){
 
+            }
+            System.out.println("8");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0;i < cathetusLength;i++){
+            for(int y = 0;y <= i;y++){
+                result.append("8");
+            }
+            result.append(System.lineSeparator());
+        }
+        System.out.println(result.toString());
 
     }
 }
